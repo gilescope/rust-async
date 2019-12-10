6ad111eb98229e9c9c7188eae8c5b53e9d9e8101
Filip Bucek <fbucek@invloop.cz>
2019-12-11 00:09:16 +0100

    added comments

--- tokio-async/src/main.rs
+++ tokio-async/src/main.rs
@@ -1,6 +1,18 @@
 use tokio::net::TcpListener;
 use tokio::prelude::*;
 
+
+
+/// Bind to IP and port and starts listening
+/// 
+/// It read socket and return same data as response
+/// 
+/// # Bash test
+/// 
+/// `nc localhost 8080` -> it will open connection -> possible to sand any data.alloc
+/// 
+///  1. When connection is estabilished -> "Connection estabilished" is printed
+/// 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut listener = TcpListener::bind("127.0.0.1:8080").await?;
