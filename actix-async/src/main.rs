1b31332e26e30ad6144cb4eacced6002557241b9
Filip Bucek <fbucek@invloop.cz>
2019-12-16 15:37:32 +0100

    trying to create controlled runner

--- actix-async/src/main.rs
+++ actix-async/src/main.rs
@@ -3,6 +3,9 @@ use actix_web::{get, App, HttpServer, Responder, web};
 use std::time::Duration;
 use tokio::{task, time, process::Command};
 
+#[macro_use]
+extern crate log;
+
 #[get("/{id}/{name}/index.html")]
 async fn index_id_name(info: web::Path<(u32, String)>) -> impl Responder {
     format!("Hello {}! id:{}\n", info.1, info.0)
@@ -13,13 +16,29 @@ async fn index() -> &'static str {
     "hello\n"
 }
 
+#[derive(Default, Debug)]
+struct Controller {
+    counter: std::sync::Mutex<i32>,
+    // number: i32,
+}
+
+impl Controller {
+    pub async fn run(&mut self) -> Result<(), std::io::Error> {
+        let mut interval = time::interval(Duration::from_secs(1));
+        loop {
+            interval.tick().await;
+            info!("Running command");
+            Command::new("date").spawn()?.await?;
+        } 
+    }
+}
 
 /// Prints output to 
 async fn dating() -> Result<(), std::io::Error> {
     let mut interval = time::interval(Duration::from_secs(1));
     loop {
         interval.tick().await;
-        println!("Running command");
+        info!("Running command");
         Command::new("date").spawn()?.await?;
     }
 }
@@ -27,8 +46,14 @@ async fn dating() -> Result<(), std::io::Error> {
 // #[tokio::main]
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-
+    std::env::set_var("RUST_LOG","debug,actix-async=trace");
+    env_logger::init();
     task::spawn(dating());
+
+    let mut control = Controller::default();
+    task::spawn(async move {
+        control.run().await
+    });
 
     println!("Starting web server");
 
