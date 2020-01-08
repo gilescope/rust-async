30f1eab7a79553ba1d49628398844446c0261dd8
Giles Cope <gilescope@gmail.com>
2020-01-08 08:05:23 +0000

    not needed

--- tokio-async/src/solution2.rs
+++
@@ -1,9 +0,0 @@
-use tokio::process::Command;
-
-#[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-    for _ in 1..=10 {
-        Command::new("echo").arg("Hello, world!").spawn()?.await?;
-    }
-    Ok(())
-}
