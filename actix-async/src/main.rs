1f8fb1da359e66acb101768f1f8132f07c7540dc
Giles Cope <gilescope@gmail.com>
2020-01-08 19:48:18 +0000

    A little less

--- actix-async/src/main.rs
+++ actix-async/src/main.rs
@@ -1,13 +1,8 @@
 use actix_async::*;
-use actix_web::HttpServer;
 use std::sync::{Arc, Mutex};
 #[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    let (_sender, receiver) = tokio::sync::mpsc::channel(10);
+async fn main() {
+    let (_, receiver) = tokio::sync::mpsc::channel(1);
     let receiver = Arc::new(Mutex::new(receiver));
-    tokio::spawn(async {
-        let mut service_controller = ServiceController::new(receiver);
-        service_controller.run().await;
-    });
-    Ok(())
+    tokio::spawn(async { ServiceController::new(receiver).run().await; });
 }
