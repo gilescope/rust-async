fba6e943be7a07c70159feca75d81c7e4db23ca5
Giles Cope <gilescope@gmail.com>
2020-01-08 07:53:49 +0000

    min step 1

--- actix-async/src/main.rs
+++ actix-async/src/main.rs
@@ -1,88 +1,40 @@
 use actix_web::{get, web, App, Error as ActixError, HttpResponse, HttpServer, Responder};
 use std::sync::{Arc, Mutex};
-use std::*;
-
 #[macro_use]
 extern crate log;
-
 use actix_async::*;
-
 #[get("/{id}/{name}/index.html")]
 async fn index_id_name(info: web::Path<(u32, String)>) -> impl Responder {
-    format!("Hello {}! id:{}\n", info.1, info.0)
+    unimplemented!()
 }
-
 #[get("/api/stop")]
 async fn api_stop(
     sender: web::Data<Arc<Mutex<sync::mpsc::Sender<Message>>>>,
 ) -> Result<HttpResponse, ActixError> {
-    trace!("{:?}", sender);
-    let sender = sender.lock().unwrap();
-    sender.send(Message::RunCheck).unwrap_or_else(|err| {
-        error!(
-            "Not possible to send message -> RunCheck - error: {:?}",
-            err
-        )
-    });
-    // if let Err(err) = sender.send(Message::RunCheck).unwrap() {
-    //     error!("Not possible to send message -> RunCheck");
-    // }
-    let text = "Shoud stop task\n".to_string();
-    Ok(HttpResponse::Ok().body(text))
+    unimplemented!()
 }
-
 #[get("/index.html")]
 async fn index() -> &'static str {
-    "hello\n"
+    unimplemented!()
 }
-
-#[derive(Default, Debug)]
-struct Check {
-    ip: String,
-    port: String,
-}
-
-// #[tokio::main]
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "debug,actix_async=trace");
     env_logger::init();
-
-    // Create sender and receiver to communicate with loop
     let (sender, receiver) = tokio::sync::mpsc::channel(10);
-    let sender = Arc::new(Mutex::new(sender)); // <-- Actix loop
-    let sender_exit = Arc::clone(&sender); // <-- Ctrl+C handler
+    let sender = Arc::new(Mutex::new(sender));
+    let sender_exit = Arc::clone(&sender);
     let receiver = Arc::new(Mutex::new(receiver));
     let receiver_tokio = Arc::clone(&receiver);
     let receiver_tokio2 = Arc::clone(&receiver);
     let receiver_tokio3 = Arc::clone(&receiver);
-
-    // Gracefull shutdown -> SIGTERM received -> send message terminate
-    ctrlc::set_handler(move || {
-        let mut sender = sender_exit.lock().expect("not possible to lock");
-        for _ in 0..4 {
-            info!("sending terminate mesage");
-            sender
-                .try_send(Message::Terminate)
-                .expect("not possible to send terminate message");
-        }
-    })
-    .expect("Error setting Ctrl+C handler");
-
+    ctrlc::set_handler(move || unimplemented!()).expect("Error setting Ctrl+C handler");
     let mut service_controller = ServiceController::new(Arc::clone(&receiver));
     service_controller
         .run()
         .await
         .expect("Not possible to run thread loop");
-
-    tokio::spawn(async move {
-        let mut service_controller = ServiceController::new(receiver_tokio);
-        service_controller
-            .run()
-            .await
-            .expect("Not possible to run thread loop");
-    });
-
+    tokio::spawn(async move { unimplemented!() });
     tokio::spawn(async move {
         let mut service_controller = ServiceController::new(receiver_tokio2);
         service_controller
@@ -90,38 +42,11 @@ async fn main() -> std::io::Result<()> {
             .await
             .expect("Not possible to run thread loop");
     });
-
-    // TODO: try this: https://users.rust-lang.org/t/new-with-async-how-to-structure-application/35233/4
-    // tokio::spawn(async move {
-    //     loop {
-    //         let message = receiver.lock().unwrap().recv().await.unwrap();
-    //         trace!("message received {:?}", &message);
-    //         match message {
-    //             Message::RunCheck => {
-    //                 info!("separtate tokio::spawn: now should be able to run task");
-    //             }
-    //             Message::Terminate => {
-    //                 info!("separtate tokio::spawn: now terminating project");
-    //                 break; // loop
-    //             }
-    //         }
-    //     }
-    //     trace!("tokio loop finishes");
-    // });
-
     info!("Starting web server");
-    // async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let res = HttpServer::new(move || {
-        App::new()
-            .service(index_id_name)
-            .service(index)
-            .service(api_stop)
-            .data(Arc::clone(&sender))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await;
-
+    let res = HttpServer::new(move || unimplemented!())
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await;
     info!("Server finished");
     res
 }
